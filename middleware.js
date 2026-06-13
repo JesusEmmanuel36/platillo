4230db6ebcb6be73e152c014552e17a10362c00a
Jesús <jesuseestrada30@gmail.com>
2026-06-12 22:52:55 -0600

    fix redireccion de api

--- middleware.js
+++ middleware.js
@@ -1,4 +1,3 @@
-
 import { NextResponse } from "next/server";
 
 const REAL_ROUTES = ["/", "/gordo"];
@@ -7,22 +6,27 @@ export function middleware(req) {
   const host = req.headers.get("host");
   const { pathname } = req.nextUrl;
 
-  // 1. Solo aplicar en platillo.mx
+  // Permitir APIs siempre
+  if (pathname.startsWith("/api")) {
+    return NextResponse.next();
+  }
+
+  // Solo aplicar en platillo.mx
   const isMainDomain = host === "platillo.mx";
 
   if (!isMainDomain) {
     return NextResponse.next();
   }
 
-  // 2. Permitir rutas reales
+  // Permitir rutas reales
   if (REAL_ROUTES.includes(pathname)) {
     return NextResponse.next();
   }
 
-  // 3. TODO lo demás se redirige
+  // Todo lo demás se redirige
   return NextResponse.redirect(new URL("https://platillo.mx", req.url));
 }
 
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-}; 
+};
